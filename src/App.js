0ec77436aaba919d7906c66a0be030429c9dba80
dripminister <95714820+dripminister@users.noreply.github.com>
2022-11-15 14:55:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Section from "./components/Section"
 function App() {
 
   const APIUrls = {
-    popular: `https://api.themoviedb.org/3/movie/popular?api_key=a38cc71f7352583dc61fce6d872eaf66&language=en-US&page=1`,
-    trending: `https://api.themoviedb.org/3/movie/popular?api_key=a38cc71f7352583dc61fce6d872eaf66&language=en-US&page=2`,
-    topRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=a38cc71f7352583dc61fce6d872eaf66&language=en-US&page=1`,
-    upcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=a38cc71f7352583dc61fce6d872eaf66&language=en-US&page=1`,
-    horror: `https://api.themoviedb.org/3/search/movie?api_key=a38cc71f7352583dc61fce6d872eaf66&language=en-US&query=horror&page=1&include_adult=false`
+    //Get your API KEY and URL on https://www.themoviedb.org/
+    popular: ,
+    trending: ,
+    topRated: ,
+    upcoming: ,
+    horror: 
   }
 
   return (
